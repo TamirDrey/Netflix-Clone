28ad8c919a59cb964140e1d34e194821ed87e0d7
OmerIgna <omerigna@gail.com>
2024-02-12 15:12:10 +0200

    modified:   backend/src/routes/userRouter.ts

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,22 +1,11 @@
 import express, { Router } from "express";
-import { Request, Response, NextFunction } from "express";
 import expressAsyncHandler from "express-async-handler";
 import { signup, signin } from "../controllers/user";
 
 const usersRouter: Router = express.Router();
 
-usersRouter.post(
-  "/signin",
-  expressAsyncHandler((req: Request, res: Response, next: NextFunction) => {
-    signin(req, res).catch(next);
-  })
-);
+usersRouter.post("/signin", expressAsyncHandler(signin));
 
-usersRouter.post(
-  "/signup",
-  expressAsyncHandler((req: Request, res: Response, next: NextFunction) => {
-    signup(req, res).catch(next);
-  })
-);
+usersRouter.post("/signup", expressAsyncHandler(signup));
 
 export default usersRouter;
