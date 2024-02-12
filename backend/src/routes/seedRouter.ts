ff4602d679bd1980df28bd472c6e6288ee32ae39
OmerIgna <omerigna@gail.com>
2024-02-12 15:52:48 +0200

    deleted:    backend/README.md #	modified:   backend/src/routes/seedRouter.ts

--- backend/src/routes/seedRouter.ts
+++ backend/src/routes/seedRouter.ts
@@ -1,11 +1,9 @@
 import express, { Router } from "express";
 import expressAsyncHandler from "express-async-handler";
 import seedData from "../controllers/seed";
-import { isAuth } from "../utils";
-
 
 const seedRouter: Router = express.Router();
 
-seedRouter.get("/", isAuth,expressAsyncHandler(seedData));
+seedRouter.get("/", expressAsyncHandler(seedData));
 
 export default seedRouter;
