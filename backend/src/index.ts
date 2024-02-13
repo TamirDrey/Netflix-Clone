633b72b56629f012b4fb8115ca1d06863022f826
TamirDrey <tamirdrey96@gmail.com>
2024-02-13 09:18:24 +0200

    13/2/24

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })) ;
 
 // Routes
 app.use("/api/v1/seed", seedRouter);
