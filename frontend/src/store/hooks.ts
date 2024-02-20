f04af6f5ee30b79101a62a1351f093dd5ccb9655
OmerIgna <omerigna@gail.com>
2024-02-20 13:31:48 +0200

    frontend/src/store/hooks.ts

--- frontend/src/store/hooks.ts
+++ frontend/src/store/hooks.ts
@@ -1,6 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./store";
 
-//use this instead of reular appDispatch and useSelector
+//use this instead of regular appDispatch and useSelector
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
