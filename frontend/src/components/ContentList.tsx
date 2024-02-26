2a0533683cc5a769fe927ad0b05215e067a95be4
OmerIgna <omerigna@gail.com>
2024-02-26 12:09:00 +0200

    frontend/src/components/ContentList.tsx

--- frontend/src/components/ContentList.tsx
+++ frontend/src/components/ContentList.tsx
@@ -8,9 +8,7 @@ interface ContentListProps {
 }
 
 const ContentList: React.FC<ContentListProps> = ({ data, title }) => {
-
-    console.log(data)
-
+  console.log(data);
 
   return (
     <div className="px-4 md:px-12 mt-4 space-y-8">
@@ -20,7 +18,7 @@ const ContentList: React.FC<ContentListProps> = ({ data, title }) => {
         </p>
         <div className="grid grid-cols-4 gap-2">
           {data.map((item) => (
-            <ContentCard key={item._id}  />
+            <ContentCard key={item._id} />
           ))}
         </div>
       </div>
