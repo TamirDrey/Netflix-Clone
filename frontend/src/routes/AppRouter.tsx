d02288034668efdb509defe0540e0f79057c5731
TamirDrey <tamirdrey96@gmail.com>
2024-02-28 10:23:15 +0200

    added acount menu and fix the routing aout

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -8,27 +8,35 @@ import SignIn from "../components/SignIn";
 import Error from "../components/Error";
 
 const AppRouter = () => {
-  // const isAuthenticated = useAppSelector(selectIsAuthenticated);
-  // console.log(isAuthenticated);
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+  console.log(isAuthenticated);
   return (
     <Router>
       <I18nextProvider i18n={i18n}>
         <Routes>
-          {PublicRoutes.map((routes) => (
-            <Route
-              path={routes.path}
-              key={routes.path}
-              element={<routes.element />}
-            />
-          ))}
-
-          {UserRoutes.map((routes) => (
-            <Route
-              path={routes.path}
-              key={routes.path}
-              element={<routes.element />}
-            />
-          ))}
+          {!isAuthenticated && (
+            <>
+              {PublicRoutes.map((route) => (
+                <Route
+                  path={route.path}
+                  key={route.path}
+                  element={<route.element />}
+                />
+              ))}
+              <Route path="/*" element={<SignIn />} />
+            </>
+          )}
+          {isAuthenticated && (
+            <>
+              {UserRoutes.map((route) => (
+                <Route
+                  path={route.path}
+                  key={route.path}
+                  element={<route.element />}
+                />
+              ))}
+            </>
+          )}
         </Routes>
       </I18nextProvider>
     </Router>
