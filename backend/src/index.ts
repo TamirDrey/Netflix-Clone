ae6d5ebfc90fa79a7723a0c4a45744821ecc80aa
TamirDrey <tamirdrey96@gmail.com>
2024-05-16 20:39:00 +0300

    new

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'https://netflix-clone-six-ruby.vercel.app' }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
